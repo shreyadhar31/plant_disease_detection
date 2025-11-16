ff77433e29ed5b979d6b8bc28d57f3079b8c8966
shreyadhar31 <143881928+shreyadhar31@users.noreply.github.com>
2025-11-16 10:53:11 +0530

    Refactor image variable name for clarity

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ class_names = [
 uploaded_file = st.file_uploader("📸 Upload Leaf Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
-    image = Image.open(uploaded_file).convert("RGB")
-st.image(image, caption="Uploaded Image", use_column_width=True)
+    img = Image.open(uploaded_file).convert("RGB")
+st.image(img, caption="Uploaded Image", use_column_width=True)
 
 st.markdown("### 🔄 Processing Image…")
 with st.spinner("AI is analyzing the leaf. Please wait..."):
